74c15919c872aa65482585f0f804ecb2cf6344b0
Himanshu Gupte <himanshugupte13@gmail.com>
2021-12-06 12:30:48 +0530

    feat: add config client settings for user-service

--- user-service/src/main/java/com/jpop8/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/jpop8/userservice/UserServiceApplication.java
@@ -1,13 +1,40 @@
 package com.jpop8.userservice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
+@RestController
 public class UserServiceApplication {
+	
+	@Value("${bookservice.provider.url}")
+	private String url;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+	
+	@GetMapping("/getBooks")
+	public void getBooks() {
+		List forObject = restTemplate.getForObject(url, List.class);
+		System.out.println(forObject);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
